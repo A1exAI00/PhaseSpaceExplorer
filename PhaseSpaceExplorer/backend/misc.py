2bf02f53f00574e282dedb93acd470581d016d35
Alex <lksndrakinin@gmail.com>
2025-09-19 22:12:49 +0300

    Add tests to flatten function

--- PhaseSpaceExplorer/backend/misc.py
+++ PhaseSpaceExplorer/backend/misc.py
@@ -59,7 +59,7 @@ def translate_array_to_periodic_segment(
     return translated_vector
 
 
-def flatten(to_flatten: list):
+def flatten(to_flatten):
     return [x for to_flatten_sub in to_flatten for x in to_flatten_sub]
 
 
@@ -84,8 +84,25 @@ def test_translate_vector_to_periodic_segment():
     assert max(abs(translate_array_to_periodic_segment(
         array([8.5, 9.5, 11.5]), -5, 2) - array([-5.5, -4.5, -2.5]))) < 1e-3
 
+def test_flatten():
+    to_flatten = [[1,2,3], [4,5]]
+    flattened = flatten(to_flatten)
+    # print(flattened)
+    assert flattened[0] == 1
+    assert flattened[1] == 2
+    assert flattened[2] == 3
+    assert flattened[3] == 4
+    assert flattened[4] == 5
+
+    to_flatten = [[], [1,2,3]]
+    flattened = flatten(to_flatten)
+    # print(flattened)
+    assert flattened[0] == 1
+    assert flattened[1] == 2
+    assert flattened[2] == 3
 
 ################################################################################
 if __name__ == "__main__":
     test_translate_value_to_periodic_segment()
     test_translate_vector_to_periodic_segment()
+    test_flatten()
